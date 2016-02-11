2d83437916783725a2bb480783cc5c6b36a6fe8c
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-02-11 22:11:12 +0300

    Manual control

--- app/components/device-car.js
+++ app/components/device-car.js
@@ -16,7 +16,7 @@ export default Ember.Component.extend({
     this.setReady();
   },
 
-  updateImage: function(status) {
+  updateStatus: function(status) {
     this.set('status', status);
     this.update();
   },
@@ -33,34 +33,33 @@ export default Ember.Component.extend({
     this.set('ready', true);
     var self = this;
 
-
     setTimeout(function() {
       // TODO - ip
-      var socket = new WebSocket("ws://localhost:2500/turtle");
+      var socket = new WebSocket("ws://localhost:2500/turtle" + self.getId());
       self.set('socket', socket);
       socket.onopen = function (event) {
-        self.updateImage('ok');
-        self.updateSensors();
-        socket.send(self.get('sensors'));
+        self.updateStatus('ok');
+        socket.send('wait');
       };
 
       socket.onmessage = function (event) {
-        self.command(event.data);
-        self.updateSensors();
-        socket.send(self.get('sensors'));
         console.log(event.data);
+        self.command(event.data);
+        if(event.data.startsWith("S")) {
+          self.execCommand(event.data.substr(1,event.data.length));
+        }
       };
 
       socket.onerror = function (event) {
-        self.updateImage('error');
+        self.updateStatus('error');
       };
 
       socket.onclose = function (event) {
-        self.updateImage('error');
+        self.updateStatus('error');
       };
 
 
-    }, Math.random() * 3000);
+    }, Math.random() * 100);
 
   },
 
@@ -73,37 +72,114 @@ export default Ember.Component.extend({
     this.set('angle', angle);
   },
 
-  getImage() {
-    return this.get('img')
+  draw: function(field) {
+    var ctx = field.getContext("2d");
+    ctx.fillStyle = '#DAB218';
+    var position = this.getPosition();
+    var xy = position[0];
+    var angle = this.get('angle');
+    var size = this.get('imgSize');
+    var siz = this.get('imgSize') / 2;
+    var deltaSize = 10;
+
+    var dy = Math.abs(Math.round(Math.cos(angle)));
+    var dx = Math.abs(Math.round(Math.sin(angle)));
+
+    ctx.fillRect(xy[0] - size / 2 + dx * deltaSize, xy[1] - size / 2 + dy * deltaSize,
+       size - dx * deltaSize * 2, size - dy * deltaSize * 2);
+
+    ctx.beginPath();
+    var status = this.get('status');
+    if(status == 'power') {
+      ctx.fillStyle = 'green';
+    }
+    if(status == 'error') {
+      ctx.fillStyle = 'red';
+    }
+    if(status == 'ok') {
+      ctx.fillStyle = 'blue';
+    }
+    ctx.arc(xy[0], xy[1], size * 0.2, 0, 2 * Math.PI, false);
+    ctx.fill();
   },
 
-  command: function(command) {
+  execCommand: function(command) {
+    console.log(command);
     if(command == 'f')
-      this.move();
-    if(command == 'r' || command == 'l') {
-      this.get('socket').close();
-      this.rotate('l');
-      this.rotate(command);
-    }
-    if(command == 's')
-      this.get('socket').close();
-    if(command == 'b') {
-      this.rotate('l');
-      this.rotate('l');
-    }
+      this.moveAnimate()
+    if(command == 'l' || command == 'r')
+      this.rotateAnimate(command);
+  },
+
+  moveAnimate: function() {
+    var self = this;
+    var count = 0;
+    var intervalId = setInterval(function() {
+      if(count >= 100) {
+        clearInterval(intervalId);
+        return;
+      }
+      count += 1
+      self.moveForward();
+      self.update();
+    }, 10)
+  },
+
+  rotateAnimate: function(direction) {
+    // var self = this;
+    // var count = 0;
+    // var dAngle = Math.PI/30;
+    // var intervalId = setInterval(function() {
+    //   console.log('int');
+    //   if(count >= 30) {
+    //     clearInterval(intervalId);
+    //     return;
+    //   }
+    //   count += 1;
+    //   self.set('angle', self.get('angle') + dAngle);
+    //   self.update();
+    // }, 500)
+    this.rotate(direction);
     this.update();
   },
 
   rotate: function(direction) {
     var xy = this.get('position');
-    this.move(28)
+    if(direction == 'r')
+      this.set('angle', this.get('angle') + Math.PI / 2);
+    if(direction == 'l')
+      this.set('angle', this.get('angle') - Math.PI / 2);
+  },
+
+
+  // by algorithm
+  command: function(command) {
+    if(command == 'f')
+      this.moveForward();
+    if(command == 'r' || command == 'l') {
+      this.get('socket').close();
+      this.rotateMove('l');
+      this.rotateMove(command);
+    }
+    if(command == 's')
+      this.get('socket').close();
+    if(command == 'b') {
+      this.rotateMove('l');
+      this.rotateMove('l');
+    }
+    this.update();
+  },
+
+  rotateMove: function(direction) {
+    var xy = this.get('position');
+    this.moveForward(28)
     if(direction == 'r')
       this.set('angle', this.get('angle') + Math.PI / 2)
     if(direction == 'l')
       this.set('angle', this.get('angle') - Math.PI / 2)
   },
 
-  move: function(step = 1) {
+  moveForward: function(step = 1) {
     var angle = this.get('angle');
     var dx = Math.round(Math.cos(angle));
     var dy = Math.round(Math.sin(angle));
@@ -153,35 +229,44 @@ export default Ember.Component.extend({
     this.set('sensors', sensors)
   },
 
-  draw: function(field) {
-    var ctx = field.getContext("2d");
-    ctx.fillStyle = '#DAB218';
-    var position = this.getPosition();
-    var xy = position[0];
-    var angle = this.get('angle');
-    var size = this.get('imgSize');
-    var siz = this.get('imgSize') / 2;
-    var deltaSize = 10;
+  setReadyA: function() {
+    this.set('ready', true);
+    var self = this;
 
-    var dy = Math.abs(Math.round(Math.cos(angle)));
-    var dx = Math.abs(Math.round(Math.sin(angle)));
 
-    ctx.fillRect(xy[0] - size / 2 + dx * deltaSize, xy[1] - size / 2 + dy * deltaSize,
-       size - dx * deltaSize * 2, size - dy * deltaSize * 2);
+    setTimeout(function() {
+      // TODO - ip
+      var socket = new WebSocket("ws://localhost:2500/turtle");
+      self.set('socket', socket);
+      socket.onopen = function (event) {
+        self.updateStatus('ok');
+        self.updateSensors();
+        socket.send(self.get('sensors'));
+      };
 
-    ctx.beginPath();
-    var status = this.get('status');
-    if(status == 'power') {
-      ctx.fillStyle = 'green';
-    }
-    if(status == 'error') {
-      ctx.fillStyle = 'red';
-    }
-    if(status == 'ok') {
-      ctx.fillStyle = 'blue';
-    }
-    ctx.arc(xy[0], xy[1], size * 0.2, 0, 2 * Math.PI, false);
-    ctx.fill();
+      socket.onmessage = function (event) {
+        self.command(event.data);
+        if(event.data.startsWith("S")) {
+          event.data('')
+        }
+          self.updateSensors();
+        socket.send(self.get('sensors'));
+        console.log(event.data);
+
+      };
+
+      socket.onerror = function (event) {
+        self.updateStatus('error');
+      };
+
+      socket.onclose = function (event) {
+        self.updateStatus('error');
+      };
+
+
+    }, Math.random() * 3000);
+
   },
+
 
 });
